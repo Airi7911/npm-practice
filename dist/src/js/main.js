6b85ad6da511804f5ad8f215574068ec99b8703d
Airi Ichikawa <mmsrm.7911@gmail.com>
2024-12-12 12:43:43 +0900

    add: sass

--- dist/src/js/main.js
+++ dist/src/js/main.js
@@ -7,26 +7,73 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://npm-practice/./src/style.scss?");
+
+/***/ }),
 
 /***/ "./src/js/index.js":
 /*!*************************!*\
   !*** ./src/js/index.js ***!
   \*************************/
-/***/ (function() {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("\n\n//# sourceURL=webpack://npm-practice/./src/js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../style.scss */ \"./src/style.scss\");\n\n\n//# sourceURL=webpack://npm-practice/./src/js/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/index.js");
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path"); // pathモジュールを読み込む
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/js/index.js",
+  entry: 
+    "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, "./dist"),
     filename: "./src/js/main.js",
+    path: path.resolve(__dirname, "./dist"),
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
@@ -18,7 +20,22 @@ module.exports = {
     open: true, // ブラウザを自動的に開くかどうか
     hot: true, // ホットリロードを有効に
   },
+  
+  module: {
+    rules: [
+      {
+        //拡張子 .scss、.sass、css を対象
+        test: /\.(scss|sass|css)$/i, 
+        // 使用するローダーの指定（後ろから順番に適用される）
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ], 
+      },
+    ]
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      // 抽出する CSS のファイル名
+      filename: 'style.css',
+    }),
     new HtmlWebpackPlugin({
       title: "Try webpack",
       template: path.resolve(__dirname, "./src/index.html"),
